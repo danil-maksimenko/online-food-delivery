025c3eb0e5703a17b3a1bd900dd6bba977ef677f
Maksymenko Danylo <danilmaksimenko000@gmail.com>
2025-03-31 21:32:51 +0200

    sidebar

--- src/main.tsx
+++ src/main.tsx
@@ -5,7 +5,7 @@ import { RouterProvider, createBrowserRouter } from "react-router";
 import { Menu } from "./pages/Menu/Menu";
 import { Cart } from "./pages/Cart/Cart";
 import { Error } from "./pages/Error/Error";
-import { Layout } from "./layout/Menu/Menu";
+import { Layout } from "./layout/Layout/Layout";
 
 const router = createBrowserRouter([
   {
