46e27f86416582a2bd9f8df086599c0f83099191
Maksymenko Danylo <danilmaksimenko000@gmail.com>
2025-03-31 20:16:57 +0200

    initial routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,10 @@
 import { MouseEvent, useState } from "react";
 import Button from "./components/Button/Button";
 import Input from "./components/Input/Input";
+import { Route, Routes } from "react-router";
+import { Menu } from "./pages/Menu/Menu";
+import { Cart } from "./pages/Cart/Cart";
+import { Error } from "./pages/Error/Error";
 
 function App() {
   const [counter, setCounter] = useState(0);
@@ -18,6 +22,15 @@ function App() {
         Кнопка
       </Button>
       <Input placeholder="Email" />
+      <div>
+        <a href="/">Меню</a>
+        <a href="/cart">Корзина</a>
+      </div>
+      <Routes>
+        <Route path="/" element={<Menu />} />
+        <Route path="/cart" element={<Cart />} />
+        <Route path="*" element={<Error />} />
+      </Routes>
     </>
   );
 }
