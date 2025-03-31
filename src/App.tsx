bdf6d31c4c8593a6b35acf4cb417e4bdeeafccc3
Maksymenko Danylo <danilmaksimenko000@gmail.com>
2025-03-31 20:24:12 +0200

    routing migration

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,25 @@
 import { MouseEvent, useState } from "react";
 import Button from "./components/Button/Button";
 import Input from "./components/Input/Input";
-import { Route, Routes } from "react-router";
+import { createBrowserRouter, RouterProvider } from "react-router";
 import { Menu } from "./pages/Menu/Menu";
 import { Cart } from "./pages/Cart/Cart";
 import { Error } from "./pages/Error/Error";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Menu />,
+  },
+  {
+    path: "/cart",
+    element: <Cart />,
+  },
+  {
+    path: "*",
+    element: <Error />,
+  },
+]);
 
 function App() {
   const [counter, setCounter] = useState(0);
@@ -26,11 +41,7 @@ function App() {
         <a href="/">Меню</a>
         <a href="/cart">Корзина</a>
       </div>
-      <Routes>
-        <Route path="/" element={<Menu />} />
-        <Route path="/cart" element={<Cart />} />
-        <Route path="*" element={<Error />} />
-      </Routes>
+      <RouterProvider router={router} />
     </>
   );
 }
