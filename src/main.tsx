4be443b232dcda5cfc2b2f106a651f3c758fd8a1
Maksymenko Danylo <danilmaksimenko000@gmail.com>
2025-04-04 20:51:23 +0200

    private routes and saving in localStorage

--- src/main.tsx
+++ src/main.tsx
@@ -11,13 +11,18 @@ import { PREFIX } from "./helpers/API";
 import { AuthLayout } from "./layout/Auth/AuthLayout";
 import { Login } from "./pages/Login/Login";
 import { Register } from "./pages/Register/Register";
+import { RequireAuth } from "./helpers/RequireAuth";
 
 const Menu = lazy(() => import("./pages/Menu/Menu"));
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <Layout />,
+    element: (
+      <RequireAuth>
+        <Layout />
+      </RequireAuth>
+    ),
     children: [
       {
         path: "/",
